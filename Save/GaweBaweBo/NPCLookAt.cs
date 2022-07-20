31f0762781636165f497df82680003dc4c718432
kangminhun <96628581+kangminhun@users.noreply.github.com>
2022-07-20 17:42:53 +0900

    Update NPCLookAt.cs

--- Save/GaweBaweBo/NPCLookAt.cs
+++ Save/GaweBaweBo/NPCLookAt.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCLookAt : Interact
 {
-    Quaternion startRotation;
     public GameObject me;
     public GameObject gameCanvas;
+    public GameObject tempChatCanvas;
+    public GameObject previewCanvas;
     public Transform point_One;
     public Transform point_Two;
     public Transform point_Three;
@@ -15,40 +14,61 @@ public class NPCLookAt : Interact
     private Camera playerCamera;
     public void Start()
     {
-        startRotation = me.transform.rotation;
+        previewCanvas = GameObject.Find("PreviewCanvas").gameObject;
     }
     protected override void OnTriggerEnter(Collider other)
     {
-        gameCanvas = other.GetComponent<ChatManager>().uiCanvas.gameObject.transform.Find("GaBaBoGame").gameObject;
-        base.OnTriggerEnter(other);
+        if (other.tag == "Player")
+        {
+            gameCanvas = other.GetComponent<ChatManager>().uiCanvas.gameObject.transform.Find("GaBaBoGame").gameObject;
+            tempChatCanvas = other.GetComponent<ChatManager>().uiCanvas.gameObject;
+            base.OnTriggerEnter(other);
+        }
     }
     public void OnTriggerStay(Collider other)
     {
-        Vector3 dir = other.transform.position - me.transform.position;
-        //수정 코드
-        Quaternion tempQue = Quaternion.Lerp(me.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 2);
-        me.transform.rotation = Quaternion.Euler(new Vector3(0, tempQue.eulerAngles.y, 0));
-        player = other.gameObject;
+        if (other.tag == "Player")
+        {
+            Vector3 dir = other.transform.position - me.transform.position;
+            //수정 코드
+            Quaternion tempQue = Quaternion.Lerp(me.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 2);
+            me.transform.rotation = Quaternion.Euler(new Vector3(0, tempQue.eulerAngles.y, 0));
+            player = other.gameObject;
+        }
     }
     protected override void OnTriggerExit(Collider other)
     {
-        me.transform.rotation = startRotation;
         cameraMove = false;
         base.OnTriggerExit(other);
     }
 
     protected override void InteractClick()
     {
-        player.GetComponent<PlayerAnimator>().agent.ResetPath();
+
+        //ui 비활성화
+
+        previewCanvas.SetActive(false);
+        tempChatCanvas.transform.Find("X_Button").gameObject.SetActive(false);
+        tempChatCanvas.transform.Find("MessageNotice").gameObject.SetActive(false);
+
+        //플레이어 이동제어
+
+        PlayerAnimator.shop = true;
+        if (player.GetComponent<PlayerAnimator>().agent.enabled)
+            player.GetComponent<PlayerAnimator>().agent.ResetPath();
         player.GetComponentInChildren<Animator>().SetBool("move", false);
+
+        //카메라
+
         playerCamera = Camera.main;
         playerCamera.transform.parent = player.transform;
-        playerCamera.cullingMask = playerCamera.cullingMask & ~(1 << LayerMask.NameToLayer("Player")); ;
-        player.transform.Find("cameraArm").gameObject.SetActive(false);
+        playerCamera.cullingMask = playerCamera.cullingMask & ~(1 << LayerMask.NameToLayer("Player"));
+        //player.transform.Find("cameraArm").gameObject.SetActive(false);
         cameraMove = true;
-        PlayerAnimator.shop = true;
-        gameCanvas.SetActive(true);
+
+        //NPC Animation
         GetComponentInChildren<Animator>().SetBool("insa", true);
+
         OffActiveImage();
     }
     public void FixedUpdate()
@@ -57,6 +77,10 @@ public class NPCLookAt : Interact
         {
             playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, point_Two.position, Time.deltaTime * 2f);
             playerCamera.transform.LookAt(point_Three.position );
+            if(Vector3.Distance(playerCamera.transform.position, point_Two.position)<=0.2f)
+            {
+                gameCanvas.SetActive(true);
+            }
         }
     }
 }
