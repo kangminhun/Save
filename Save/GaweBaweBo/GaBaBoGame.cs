214fc7cdc912f62961765e282f1e120454c3138f
kangminhun <96628581+kangminhun@users.noreply.github.com>
2022-07-20 17:41:45 +0900

    Update GaBaBoGame.cs

--- Save/GaweBaweBo/GaBaBoGame.cs
+++ Save/GaweBaweBo/GaBaBoGame.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GaBaBoGame : MonoBehaviour
 {
-    public GameObject game;
-    public GameObject gameGM;
+    public GameUi mainUi;
     private Camera playerCamera;
     private GameObject player;
     private GameObject npc;
@@ -22,25 +21,31 @@ public class GaBaBoGame : MonoBehaviour
         playerCamera.transform.position = npclookat.point_One.position;
         npclookat.cameraMove = false;
         gameObject.SetActive(false);
-        game.SetActive(true);
-        //                   도박 ㄱ???
-        //if (PointScore.instance.scoreValue >= 100)
-        //{
-        //    PointScore.instance.PointDown(100);
-        //    gameObject.SetActive(false);
-        //    game.SetActive(true);
-        //}
-        //else
-        //    return;
+        mainUi.UiOn(1);
     }
     public void End()
     {
+
+        //ui활성화
+
+        npclookat.previewCanvas.SetActive(true);
+        npclookat.tempChatCanvas.transform.Find("X_Button").gameObject.SetActive(true);
+        npclookat.tempChatCanvas.transform.Find("MessageNotice").gameObject.SetActive(true);
+
+        //카메라
+
         player.transform.Find("cameraArm").gameObject.SetActive(true);
         playerCamera.transform.parent = player.transform.Find("cameraArm").transform.Find("ButtonCamera");
-        playerCamera.transform.position = player.transform.Find("cameraArm").transform.Find("ButtonCamera").transform.position;
-        playerCamera.transform.LookAt(player.transform.Find("cameraArm"));
+        playerCamera.transform.localPosition = new Vector3(0,0,0);
+        playerCamera.transform.localRotation = Quaternion.identity;
         playerCamera.cullingMask |= 1 << LayerMask.NameToLayer("Player");
-        gameGM.SetActive(false);
+
+        //게임 ui 끄기
+
+        mainUi.gameObject.SetActive(false);
+
+        //player 이동 제한 해제
+
         npclookat.cameraMove = false;
         PlayerAnimator.shop = false;
     }
