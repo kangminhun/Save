73ae3014921d38fc61745a6c4743e8b37b4147a1
kangminhun <96628581+kangminhun@users.noreply.github.com>
2022-10-25 19:34:00 +0900

    Create Codetest_09.cs

---
+++ CodeTest/Codetest_09.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public string solution(string phone_number) 
+    {
+        string answer="";
+        
+        string str=phone_number.Substring(phone_number.Length - 4);
+        
+        for(int i=0;i<phone_number.Length-4;++i)
+        {
+            answer+="*";
+        }
+        answer += str;
+        return answer;
+        //핸드폰 번호 가리기
+    }
+}
