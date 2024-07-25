c2aa10929fcb64ec82c5c14632d5e2733d64ef71
kangminhun <96628581+kangminhun@users.noreply.github.com>
2024-07-25 20:31:26 +0900

    Create Codetest_11.cs

---
+++ CodeTest/Codetest_11.cs
@@ -0,0 +1,20 @@
+//문제 : 정수 a와 b가 주어집니다. 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        int b = Int32.Parse(s[1]);
+
+        Console.WriteLine($"a = {a}");
+        Console.WriteLine($"b = {b}");
+    }
+}
