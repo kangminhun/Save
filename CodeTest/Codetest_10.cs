456ff66638843e3a0d5981615c958ac61149b421
kangminhun <96628581+kangminhun@users.noreply.github.com>
2024-07-25 20:30:53 +0900

    Create Codetest_10.cs

---
+++ CodeTest/Codetest_10.cs
@@ -0,0 +1,15 @@
+// 문제 : 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+        Console.WriteLine(s);
+    }
+}
